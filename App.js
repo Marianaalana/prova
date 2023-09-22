f76c09482c901e43dd02f443ae7188e98ca51fd9
Aluno <mariana.bala2b2@gmail.com>
2023-09-22 16:43:25 -0300

    branch com o código da aula do dia Fri, Sep 22, 2023  4:43:25 PM

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import LoginScreen from './src/LoginScreen'; 
+
+function App() {
+  return (
+      <LoginScreen></LoginScreen>
+    );
+}
+
+export default App;
